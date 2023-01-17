f0f5d19ede2b05e892538acd533c60c8b62c520d
thiago felipe <thiaguinho021bol@gmail.com>
2023-01-17 18:14:39 -0200

    master

--- Aula01/calc.py
+++ Aula01/calc.py
@@ -1,5 +1,5 @@
 n1= int (input('escreva um numero :'))
-n2 = int (input ('escreva um numero :') )
+n2 = int (input ('escreva um numero ::') )
 resul= (n1+n2)
 print ('seu resultado é {} + {} = {}'.format (n1,n2 ,resul))
 n1= int (input('escreva um numero '))
