500ca526a9dae89d63df6f08c598d7dcda701937
thiago felipe <thiaguinho021bol@gmail.com>
2023-01-19 17:15:11 -0200

    .

--- aula 02/tabuada_completa.py
+++ aula 02/tabuada_completa.py
@@ -5,15 +5,15 @@ from os import system, name
 
 system ('cls') if(name=='nt') else system ('clear')
 print ('-*****Tabuada*****-')
+linha = ''
+linha2 =''
 for i in range (11):
-    linha = ''
-    linha2 =''
 
     for multiplicador in range(1, 6):
         resultado = multiplicador *i  
         linha += f'{multiplicador}*{i:<2} = {resultado:<3} | '
         resultado2 = (multiplicador + 5)*i
-        linha2 += f'{multiplicador}*{i:<2} = {resultado:<3} | '
+        linha2 += f'{multiplicador+5}*{i:<2} = {resultado2:<3} | '
     linha +='\n'
     linha2 +='\n'
 print(linha)
